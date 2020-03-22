f976056f2e7a557d533de361fe1869a5787db2b8
Matthias Lenz <lenz_m497@hotmail.de>
2020-03-22 23:13:56 +0000

    splitting chat service into chatclient and chatbackend

--- chatclient/app/__init__.py
+++ chatclient/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
-socketio = SocketIO(cors_allowed_origins="*", engineio_logger=False)
+socketio = SocketIO()
 
 
 def create_app(debug=False):
