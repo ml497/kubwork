addfa727ab3449ca803a9b35fc2b996128df62e7
Matthias Lenz <matthias.lenz@gamesys.co.uk>
2020-03-25 14:04:40 +0000

    Fixing docker connectivity issues

--- chatbackend/chat.py
+++ chatbackend/chat.py
@@ -1,7 +1,10 @@
 #!/bin/env python
 from app import create_app, socketio
+import os
 
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    port = int(os.environ.get("PORT", 5000))
+
+    socketio.run(app, debug=True, host='0.0.0.0', port=port)
